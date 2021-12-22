d105d280b5d0651a8655368d56dd96f5ba38ca61
Congling XIA <xiacongling@xiaomi.com>
2021-12-22 08:37:56 +0800

    resolve 686

--- src/main/scala/me/xiacongling/code/scala/leet/k0/h6/s0680_0689.scala
+++ src/main/scala/me/xiacongling/code/scala/leet/k0/h6/s0680_0689.scala
@@ -3,6 +3,44 @@ package me.xiacongling.code.scala.leet.k0.h6
 import me.xiacongling.code.scala.leet.{Difficulty, Solution}
 
 object s0680_0689 {
+  @Solution(id = 686, title = "重复叠加字符串", difficulty = Difficulty.Medium, description =
+    """给定两个字符串 a 和 b，寻找重复叠加字符串 a 的最小次数，使得字符串 b 成为叠加后的字符串 a 的子串，
+      |如果不存在则返回 -1。
+      |
+      |注意：字符串 "abc" 重复叠加 0 次是 ""，重复叠加 1 次是 "abc"，重复叠加 2 次是 "abcabc"。
+      |
+      |示例 1：
+      |输入：a = "abcd", b = "cdabcdab"
+      |输出：3
+      |解释：a 重复叠加三遍后为 "abcdabcdabcd", 此时 b 是其子串。
+      |
+      |示例 2：
+      |输入：a = "a", b = "aa"
+      |输出：2
+      |
+      |示例 3：
+      |输入：a = "a", b = "a"
+      |输出：1
+      |
+      |示例 4：
+      |输入：a = "abc", b = "wxyz"
+      |输出：-1
+      |
+      |提示：
+      |* 1 <= a.length <= 10^4
+      |* 1 <= b.length <= 10^4
+      |* a 和 b 由小写英文字母组成
+      |
+      |来源：力扣（LeetCode）
+      |链接：https://leetcode-cn.com/problems/repeated-string-match
+      |著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+      |""".stripMargin)
+  def repeatedStringMatch(a: String, b: String): Int = {
+    val n: Int = b.length / a.length + (if (b.length % a.length == 0) 0 else 1)
+    val shortest: String = a * n
+    if (shortest contains b) n else if ((shortest + a) contains b) n + 1 else -1
+  }
+
   @Solution(id = 689, title = "三个无重叠子数组的最大和", difficulty = Difficulty.Hard, description =
     """给你一个整数数组 nums 和一个整数 k ，找出三个长度为 k 、互不重叠、且
       |3 * k 项的和最大的子数组，并返回这三个子数组。
--- src/test/scala/me/xiacongling/code/scala/leet/k0/h6/s0680_0689Test.scala
+++ src/test/scala/me/xiacongling/code/scala/leet/k0/h6/s0680_0689Test.scala
@@ -1,6 +1,6 @@
 package me.xiacongling.code.scala.leet.k0.h6
 
-import me.xiacongling.code.scala.leet.k0.h6.s0680_0689.maxSumOfThreeSubarrays
+import me.xiacongling.code.scala.leet.k0.h6.s0680_0689.{maxSumOfThreeSubarrays, repeatedStringMatch}
 import org.junit.runner.RunWith
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should
@@ -8,6 +8,16 @@ import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class s0680_0689Test extends AnyFunSuite with should.Matchers {
+  test("Test Cases for # 686") {
+    repeatedStringMatch("abcd", "cdabcdab") should be(3)
+    repeatedStringMatch("a", "aa") should be(2)
+    repeatedStringMatch("aaa", "a") should be(1)
+    repeatedStringMatch("aa", "aaa") should be(2)
+    repeatedStringMatch("ab", "a") should be(1)
+    repeatedStringMatch("a", "a") should be(1)
+    repeatedStringMatch("abc", "wxyz") should be(-1)
+  }
+
   test("Test Cases for #689") {
     maxSumOfThreeSubarrays(Array(1, 2, 1, 2, 6, 7, 5, 1), 2) should be(Array(0, 3, 5))
     maxSumOfThreeSubarrays(Array(1, 2, 1, 2, 1, 2, 1, 2, 1), 2) should be(Array(0, 2, 4))
