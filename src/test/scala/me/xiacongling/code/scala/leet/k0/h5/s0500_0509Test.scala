644ac9cda87629badeea29abec2efa9dba46a7f0
Congling XIA <xiacongling@xiaomi.com>
2021-12-31 17:06:39 +0800

    add solution

--- src/test/scala/me/xiacongling/code/scala/leet/k0/h5/s0500_0509Test.scala
+++ src/test/scala/me/xiacongling/code/scala/leet/k0/h5/s0500_0509Test.scala
@@ -1,6 +1,6 @@
 package me.xiacongling.code.scala.leet.k0.h5
 
-import me.xiacongling.code.scala.leet.k0.h5.s0500_0509.findRelativeRanks
+import me.xiacongling.code.scala.leet.k0.h5.s0500_0509.{checkPerfectNumber, findRelativeRanks}
 import org.junit.runner.RunWith
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should
@@ -11,5 +11,13 @@ class s0500_0509Test extends AnyFunSuite with should.Matchers {
   test("Test Cases for #506") {
     findRelativeRanks(Array(5, 4, 3, 2, 1)) should be(Array("Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"))
     findRelativeRanks(Array(10, 3, 8, 9, 4)) should be(Array("Gold Medal", "5", "Bronze Medal", "Silver Medal", "4"))
+  }
+
+  test("Test Cases for #507") {
+    checkPerfectNumber(2) shouldBe false
+    checkPerfectNumber(6) shouldBe true
+    checkPerfectNumber(28) shouldBe true
+    checkPerfectNumber(496) shouldBe true
+    checkPerfectNumber(8128) shouldBe true
   }
 }
