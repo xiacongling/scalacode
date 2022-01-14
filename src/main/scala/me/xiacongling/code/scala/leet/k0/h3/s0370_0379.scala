fdc78312d01f44a7f4ebe86ac46156e583f1f7e1
Congling XIA <xiacongling@xiaomi.com>
2022-01-14 08:13:44 +0800

    add solution

--- src/main/scala/me/xiacongling/code/scala/leet/k0/h3/s0370_0379.scala
+++ src/main/scala/me/xiacongling/code/scala/leet/k0/h3/s0370_0379.scala
@@ -2,6 +2,9 @@ package me.xiacongling.code.scala.leet.k0.h3
 
 import me.xiacongling.code.scala.leet.{Difficulty, Solution}
 
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
 object s0370_0379 {
   @Solution(id = 372, title = "超级次方", difficulty = Difficulty.Medium, description =
     """你的任务是计算 ab 对 1337 取模，a 是一个正整数，b 是一个非常大的正整数且会以数组形式给出。
@@ -60,5 +63,54 @@ object s0370_0379 {
       t = pow(t, 10)
     }
     ans
+  }
+
+  @Solution(id = 373, title = "查找和最小的 K 对数字", difficulty = Difficulty.Medium, description =
+    """给定两个以升序排列的整数数组 nums1 和 nums2 , 以及一个整数 k 。
+      |
+      |定义一对值 (u,v)，其中第一个元素来自 nums1，第二个元素来自 nums2 。
+      |
+      |请找到和最小的 k 个数对 (u1,v1),  (u2,v2)  ...  (uk,vk) 。
+      |
+      |示例 1:
+      |输入: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+      |输出: [1,2],[1,4],[1,6]
+      |解释: 返回序列中的前 3 对数：
+      |     [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+      |
+      |示例 2:
+      |输入: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+      |输出: [1,1],[1,1]
+      |解释: 返回序列中的前 2 对数：
+      |     [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+      |
+      |示例 3:
+      |输入: nums1 = [1,2], nums2 = [3], k = 3
+      |输出: [1,3],[2,3]
+      |解释: 也可能序列中所有的数对都被返回:[1,3],[2,3]
+      |
+      |提示:
+      |* 1 <= nums1.length, nums2.length <= 10^4
+      |* -10^9 <= nums1[i], nums2[i] <= 10^9
+      |* nums1, nums2 均为升序排列
+      |* 1 <= k <= 1000
+      |
+      |来源：力扣（LeetCode）
+      |链接：https://leetcode-cn.com/problems/find-k-pairs-with-smallest-sums
+      |著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+      |""".stripMargin)
+  def kSmallestPairs(nums1: Array[Int], nums2: Array[Int], k: Int): List[List[Int]] = {
+    val pq: mutable.PriorityQueue[(Int, Int, Int)] = mutable.PriorityQueue()(Ordering.by(-_._1))
+    val result: ListBuffer[List[Int]] = ListBuffer()
+    Range(0, nums1.length.min(k)).foreach(i => pq.addOne((nums1(i) + nums2(0), i, 0)))
+
+    var t: Int = 0
+    while (t < k && pq.nonEmpty) {
+      val (_, i, j) = pq.dequeue()
+      result += List(nums1(i), nums2(j))
+      if (j + 1 < nums2.length) pq.addOne((nums1(i) + nums2(j + 1), i, j + 1))
+      t += 1
+    }
+    result.toList
   }
 }
--- src/test/scala/me/xiacongling/code/scala/leet/k0/h3/s0370_0379Test.scala
+++ src/test/scala/me/xiacongling/code/scala/leet/k0/h3/s0370_0379Test.scala
@@ -1,6 +1,6 @@
 package me.xiacongling.code.scala.leet.k0.h3
 
-import me.xiacongling.code.scala.leet.k0.h3.s0370_0379.superPow
+import me.xiacongling.code.scala.leet.k0.h3.s0370_0379.{kSmallestPairs, superPow}
 import org.junit.runner.RunWith
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should
@@ -13,5 +13,11 @@ class s0370_0379Test extends AnyFunSuite with should.Matchers {
     superPow(2, Array(1, 0)) should be(1024)
     superPow(1, Array(4, 3, 3, 8, 5, 2)) should be(1)
     superPow(2147483647, Array(2, 0, 0)) should be(1198)
+  }
+
+  test("Test Cases for #373") {
+    kSmallestPairs(Array(1, 7, 11), Array(2, 4, 6), 3) shouldBe List(List(1, 2), List(1, 4), List(1, 6))
+    kSmallestPairs(Array(1, 1, 2), Array(1, 2, 3), 2) shouldBe List(List(1, 1), List(1, 1))
+    kSmallestPairs(Array(1, 2), Array(3), 3) shouldBe List(List(1, 3), List(2, 3))
   }
 }
